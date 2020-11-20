997aa73b1377140c27507dfb0554808dec47ae95
Conrad Chan <conradchan@box.com>
2020-11-19 16:00:54 -0800

    feat(drawing): Include icons for toolbar

--- src/components/Popups/PopupDrawingToolbar.tsx
+++ src/components/Popups/PopupDrawingToolbar.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import classNames from 'classnames';
+import { FormattedMessage } from 'react-intl';
+import IconRedo from 'box-ui-elements/es/icon/line/Redo16';
+import IconTrash from 'box-ui-elements/es/icon/line/Trash16';
+import IconUndo from 'box-ui-elements/es/icon/line/Undo16';
+import IconToolbarGrabber16 from '../../icons/IconToolbarGrabber';
+import messages from './messages';
 import PopupBase from './PopupBase';
-import { Options, PopupReference } from './Popper';
+import { Options, PopupReference, Rect } from './Popper';
 import './PopupDrawingToolbar.scss';
 
 type Props = {
@@ -26,7 +32,7 @@ const options: Partial<Options> = {
         {
             name: 'offset',
             options: {
-                offset: [0, 8],
+                offset: ({ popper }: { popper: Rect }) => [-popper.width / 2, 8],
             },
         },
         {
@@ -50,43 +56,46 @@ const PopupDrawingToolbar = (props: Props): JSX.Element => {
             reference={reference}
         >
             {/* anchor for dragging */}
-            {/* undo button */}
-            <button
-                className="ba-PopupDrawingToolbar-undo"
-                data-testid="ba-PopupDrawingToolbar-undo"
-                disabled={!canUndo}
-                onClick={() => onUndo()}
-                type="button"
-            >
-                U
-            </button>
-            {/* redo button */}
-            <button
-                className="ba-PopupDrawingToolbar-redo"
-                data-testid="ba-PopupDrawingToolbar-redo"
-                disabled={!canRedo}
-                onClick={() => onRedo()}
-                type="button"
-            >
-                R
-            </button>
-            {/* trash button */}
-            <button
-                className="ba-PopupDrawingToolbar-delete"
-                data-testid="ba-PopupDrawingToolbar-delete"
-                onClick={() => onDelete()}
-                type="button"
-            >
-                D
-            </button>
-            {/* add comment button */}
+            <div className="ba-PopupDrawingToolbar-group">
+                <span className="ba-PopupDrawingToolbar-grabber">
+                    <IconToolbarGrabber16 />
+                </span>
+            </div>
+            <div className="ba-PopupDrawingToolbar-group">
+                <button
+                    className="ba-PopupDrawingToolbar-undo"
+                    data-testid="ba-PopupDrawingToolbar-undo"
+                    disabled={!canUndo}
+                    onClick={() => onUndo()}
+                    type="button"
+                >
+                    <IconUndo />
+                </button>
+                <button
+                    className="ba-PopupDrawingToolbar-redo"
+                    data-testid="ba-PopupDrawingToolbar-redo"
+                    disabled={!canRedo}
+                    onClick={() => onRedo()}
+                    type="button"
+                >
+                    <IconRedo />
+                </button>
+                <button
+                    className="ba-PopupDrawingToolbar-delete"
+                    data-testid="ba-PopupDrawingToolbar-delete"
+                    onClick={() => onDelete()}
+                    type="button"
+                >
+                    <IconTrash />
+                </button>
+            </div>
             <button
                 className="ba-PopupDrawingToolbar-comment"
                 data-testid="ba-PopupDrawingToolbar-comment"
                 onClick={() => onReply()}
                 type="button"
             >
-                Add Comment
+                <FormattedMessage {...messages.buttonAddComent} />
             </button>
         </PopupBase>
     );
